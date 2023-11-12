a438389721a3aaafbac397eeb865a01b2caa2766
thalieskoda <thalieskoda@hotmail.com>
2023-11-12 14:13:43 -0500

    Auth completed with fb, apple, google

--- airbnb/app/(modals)/login.tsx
+++ airbnb/app/(modals)/login.tsx
@@ -10,10 +10,46 @@ import { useWarmUpBrowser } from "@/hooks/useWarmUpBrowser";
 
 import { defaultStyles } from "../../constants/styles";
 import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { useOAuth } from "@clerk/clerk-expo";
+import { useRouter } from "expo-router";
+//wtf is an enum
+enum Strategy {
+  Google = "oauth_google",
+  Apple = "oauth_Apple",
+  Facebook = "oauth_facebook",
+}
 
 const Page = () => {
   //Make sure that it's fine on end routes
   useWarmUpBrowser();
+  const router = useRouter();
+
+  const { startOAuthFlow: googleAuth } = useOAuth({ strategy: "oauth_google" });
+  const { startOAuthFlow: appleAuth } = useOAuth({ strategy: "oauth_apple" });
+  const { startOAuthFlow: facebookAuth } = useOAuth({
+    strategy: "oauth_facebook",
+  });
+
+  const onSelectAuth = async (strategy: Strategy) => {
+    const selectedAuth = {
+      [Strategy.Google]: googleAuth,
+      [Strategy.Apple]: appleAuth,
+      [Strategy.Facebook]: facebookAuth,
+    }[strategy];
+
+    try {
+      const { createdSessionId, setActive } = await selectedAuth();
+
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+        router.back();
+      }
+    } catch (err) {
+      console.error("OAuth error", err);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -41,6 +77,46 @@ const Page = () => {
           }}
         />
       </View>
+      <View style={{ gap: 20 }}>
+        <TouchableOpacity style={styles.btnOutline}>
+          <Ionicons
+            name="call-outline"
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Phone</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Apple)}>
+          <Ionicons
+            name="md-logo-apple"
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Apple</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Google)}>
+          <Ionicons
+            name="md-logo-google"
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Google</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.btnOutline}
+          onPress={() => onSelectAuth(Strategy.Facebook)}>
+          <Ionicons
+            name="md-logo-facebook"
+            size={24}
+            style={defaultStyles.btnIcon}
+          />
+          <Text style={styles.btnOutlineText}>Continue with Facebook</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -59,6 +135,22 @@ const styles = StyleSheet.create({
     color: Colors.grey,
     gap: 10,
     alignItems: "center",
+  },
+  btnOutline: {
+    backgroundColor: "#FFF",
+    borderWidth: 1,
+    borderColor: "grey",
+    height: 50,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    paddingHorizontal: 10,
+  },
+
+  btnOutlineText: {
+    color: "#000",
+    fontSize: 16,
   },
 });
 export default Page;
